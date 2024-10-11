ecb6a688f213139c4550877fe80fcd9d6c7d2d48
Abhijana <abhijanaramanda@gmail.com>
2024-10-11 11:53:37 +0800

    feat: :sparkles: client initialization

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { getWsProvider } from "polkadot-api/ws-provider/web";
+import { createClient, type PolkadotClient } from "polkadot-api";
+
+function makeClient(endpoint: string): PolkadotClient {
+  console.info("Connecting to ", endpoint);
+  const provider = getWsProvider(endpoint);
+  const client = createClient(provider);
+  return client;
+}
+
+async function main() {
+  const polkadotClient = makeClient("wss://rpc.polkadot.io");
+  console.log({ polkadotClient });
+}
+
+main();
