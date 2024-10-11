6aa56e73a30130672eea39dabaa7438462e34f14
Abhijana <abhijanaramanda@gmail.com>
2024-10-11 12:29:08 +0800

    feat: :sparkles: get account's balance

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import { getWsProvider } from "polkadot-api/ws-provider/web";
-import { createClient, type PolkadotClient } from "polkadot-api";
+import {
+  createClient,
+  type PolkadotClient,
+  type SS58String,
+} from "polkadot-api";
+import { dot } from "@polkadot-api/descriptors";
 
 function makeClient(endpoint: string): PolkadotClient {
   console.info("Connecting to ", endpoint);
@@ -22,9 +27,23 @@ async function printChainInfo(client: PolkadotClient) {
   );
 }
 
+async function getBalance(
+  polkadotClient: PolkadotClient,
+  address: SS58String
+): Promise<BigInt> {
+  const dotApi = polkadotClient.getTypedApi(dot);
+  const accountInfo = await dotApi.query.System.Account.getValue(address);
+  const { free, reserved } = accountInfo.data;
+  return free + reserved;
+}
+
 async function main() {
   const polkadotClient = makeClient("wss://rpc.polkadot.io");
   await printChainInfo(polkadotClient);
+
+  const address = "15DCZocYEM2ThYCAj22QE4QENRvUNVrDtoLBVbCm5x4EQncr";
+  const balance = await getBalance(polkadotClient, address);
+  console.log(`Balance of account ${address} is ${balance}`);
 }
 
 main();
