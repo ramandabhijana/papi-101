e17e28355e35d8c7064a3254cfe3cfdc971fe755
Abhijana <abhijanaramanda@gmail.com>
2024-10-12 02:36:54 +0800

    feat: :sparkles: query fellowship members from collectives chain

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import {
   type PolkadotClient,
   type SS58String,
 } from "polkadot-api";
-import { dot, people } from "@polkadot-api/descriptors";
+import { collectives, dot, people } from "@polkadot-api/descriptors";
 
 function makeClient(endpoint: string): PolkadotClient {
   console.info("Connecting to ", endpoint);
@@ -50,6 +50,29 @@ async function getDisplayName(
   return (displayName as FixedSizeBinary<any>)?.asText();
 }
 
+interface FellowshipMember {
+  address: SS58String;
+  rank: number;
+}
+async function getFellowshipMembers(
+  collectivesClient: PolkadotClient
+): Promise<FellowshipMember[]> {
+  const collectivesApi = collectivesClient.getTypedApi(collectives);
+
+  const rawMembers =
+    await collectivesApi.query.FellowshipCollective.Members.getEntries();
+
+  const fellowshipMembers = rawMembers.map((m) => {
+    const member: FellowshipMember = {
+      address: m.keyArgs[0],
+      rank: m.value,
+    };
+    return member;
+  });
+
+  return fellowshipMembers;
+}
+
 async function main() {
   // Set up clients
   const polkadotClient = makeClient("wss://rpc.polkadot.io");
@@ -58,12 +81,28 @@ async function main() {
   const peopleClient = makeClient("wss://polkadot-people-rpc.polkadot.io");
   await printChainInfo(peopleClient);
 
-  // Chain interactions
-  const address = "15DCZocYEM2ThYCAj22QE4QENRvUNVrDtoLBVbCm5x4EQncr";
-  const balance = await getBalance(polkadotClient, address);
-  const display = await getDisplayName(peopleClient, address);
+  const collectivesClient = makeClient(
+    "wss://polkadot-collectives-rpc.polkadot.io"
+  );
+  await printChainInfo(collectivesClient);
 
-  console.log(`Balance of account ${address} (${display}) is ${balance}`);
+  // Chain interactions
+  const members = await getFellowshipMembers(collectivesClient);
+
+  let table = [];
+
+  for (const member of members) {
+    const { address } = member;
+
+    const balance = await getBalance(polkadotClient, address);
+    const display = await getDisplayName(peopleClient, address);
+
+    table.push({ ...member, display, balance });
+  }
+
+  table.sort((a, b) => b.rank - a.rank);
+
+  console.table(table);
 }
 
 main();
